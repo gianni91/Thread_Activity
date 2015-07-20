09fc4b2b58875d48142136a20e79f27cde50803f
gianni91 <job09001@byui.edu>
2015-07-20 16:52:59 -0600

    Latest in the thread activity

---
+++ app/src/main/java/com/example/jjob2/thread_activity/BackgroundThread.java
@@ -0,0 +1,81 @@
+package com.example.jjob2.thread_activity;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+import java.net.URL;
+
+/**
+ * Created by jjob2 on 7/2/2015.
+ */
+public class BackgroundThread extends AsyncTask<String, Integer, String>
+{
+
+    protected String doInBackground(String... params) {
+        String result = "";
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("" + i * 2);
+                if(i >= 9) {
+                    Log.e("jj","An unexpected number was printed");
+                }
+                Thread.sleep(250);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.e("jj","failure in the doInBackground function in MainActivity");
+        }
+
+        return result;
+    }
+
+    public void doInForeground() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("" + i);
+                Thread.sleep(250);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+/*
+public class BackgroundThread extends AsyncTask<URL , Integer, Long> {
+   //protected Long doInBackground(URL... urls) {
+   protected Long doInBackground(Integer... nums) {
+       long myNum = 20;
+       System.out.println("testing BackgroundThread");
+       System.out.println("1");
+       return myNum;
+   }
+
+    /// ocmment out tihs one
+    protected Long doInBackground(URL... urls) {
+        int count = urls.length;
+        long totalSize = 0;
+        for (int i = 0; i < count; i++) {
+            //totalSize += Downloader.downloadFile(urls[i]);
+            publishProgress((int) ((i / (float) count) * 100));
+            // Escape early if cancel() is called
+            if (isCancelled()) break;
+        }
+        return totalSize;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+        System.out.println("testing on progress update");
+       ;// setProgressPercent(progress[0]);
+    }
+
+    protected void onPostExecute(Long result) {
+        System.out.println("testing onPostExecute");
+        ;//showDialog("Downloaded " + result + " bytes");
+    }
+
+
+}
+*/
--- app/src/main/java/com/example/jjob2/thread_activity/MainActivity.java
+++ app/src/main/java/com/example/jjob2/thread_activity/MainActivity.java
@@ -2,8 +2,23 @@ package com.example.jjob2.thread_activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +27,131 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("jj", "In the onCreate function");
+
+        //Testing added this for the background thread
+        ProgressBar progressbar = (ProgressBar) findViewById(R.id.progressBar);
+        progressbar.setMax(100);
+
+        createFile();
+    }
+
+    //Added, testing
+    public void createFile() {
+        Log.i("jj","In the createFile function");
+        System.out.println("Testing the button");
+
+        try {
+            // Testing
+           // URL url1 = new URL("https://developer.android.com/training/multiple-threads/communicate-ui.html");
+
+
+            //END-TEST
+            //testing again
+            BackgroundThread myT = new BackgroundThread();
+            myT.doInForeground();
+
+            String nums;
+            //= "1 2 3 4 5 6 7 8 9 10";
+            File myFile = new File(this.getFilesDir(), "myFile.txt");
+
+            FileWriter myWriter = new FileWriter(myFile.getAbsoluteFile());
+            BufferedWriter myBWriter = new BufferedWriter(myWriter);
+
+//            myBWriter.write(nums);
+            for (int i = 0; i < 10; i++) {
+                myBWriter.write("" + (i + 1) + "\n");
+                Thread.sleep(250);
+            }
+            myBWriter.close();
+
+        } catch (IOException e) {                  // This exception needed for the Writer?
+            Log.e("jj","Failure to write to the file?");
+            e.printStackTrace();
+        } catch (InterruptedException e) {        // This exception is needed for Thread.sleep()
+            Log.e("jj", "Failure to perform the Thread.sleep() function?");
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(View myView)  //added Wednesday
+    {
+        /*
+        try {
+            File myFile = new File(this.getFilesDir(), "myFile.txt");
+
+            FileReader myReader = new FileReader(myFile.getAbsoluteFile());
+            BufferedReader myBReader = new BufferedReader(myReader);
+
+//            myBWriter.write(nums);
+
+            char[] words = new char[256];
+            for (int i = 0; i < 10; i++) {
+                myBReader.read(words);
+                Thread.sleep(250);
+            }
+            myBReader.close();
+
+            String sNumbers = words;
+            Log.i("jj",words[0]);
+
+        } catch (Exception e) {
+            Log.e("jj","Failure to read the file");
+        }
+        */
+
+        Log.i("jj","In the readFile function");
+        try {
+            BackgroundThread myThread = new BackgroundThread();
+            myThread.doInBackground("test1");
+//            myThread.doInForeground();
+        } catch (Exception e) {
+            Log.e("jj","Failure to read the file?");
+        }
+            //myThread.doInBackground("hi there");
+
+/*
+            ListView myListView = (ListView)findViewById(R.id.listView); //added wednesday
+            ArrayList<String> myList = new ArrayList<String> ( ) ;
+
+
+            //TEST-ADDED
+            String fileName = this.getFilesDir() + "/myFile.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                myList.add(line);  //testing (wednes)
+                Thread.sleep(250);  // added today
+            }
+
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, myList); // testing - added wednes
+            myListView.setAdapter(myAdapter);
+
+            reader.close();
+            //END-TEST-ADD
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {   // This was added for the Thread.sleep function?
+            e.printStackTrace();
+        }*/
+    }
+
+    public void clearFile(View myView)  // Added Thursday
+    {
+        Log.i("jj","In the clearFile function");
+        ListView myListView = (ListView)findViewById(R.id.listView);
+        ArrayList<String> myList = new ArrayList<String> ( ) ;
+
+        String line = "";
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, myList); // testing - added wednes
+        myListView.setAdapter(myAdapter);
     }
 
     @Override
